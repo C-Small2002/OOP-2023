899cb82dfd5d295bbef43bae13e2fa7dcb99e74e
Cathal Small <cathal.small1@gmail.com>
2023-02-08 20:50:55 +0000

    loops

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,11 +5,15 @@ import processing.core.PApplet;
 public class Loops extends PApplet {
 
 	int mode = 0;
+	float centerX = width /2;
+	float centerY = height/2;
 	public void settings() {
 		size(1000, 1000);
+
 	}
 
 	public void setup() {
+		colorMode(HSB);
 	}
 
 
@@ -22,9 +26,19 @@ public class Loops extends PApplet {
 
 	public void draw() {
 		background(0);
-		fill(255);
+		fill(230,240,210);
 		
-		
+		switch(mode){
+			case 0:
+			{
+				if(mouseX < centerX && mouseX > centerX/2){
+
+					rect(centerX, centerY, width/4, height);
+
+				}
+				
+			}
+		}
 		
 
 	}

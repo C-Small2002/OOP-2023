8ee1ca0610c6d2261287e5ca0ee4c59254b55c30
Cathal Small <cathal.small1@gmail.com>
2023-02-13 20:47:30 +0000

    star

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -9,6 +9,8 @@ public class StarMap extends PApplet
 		size(500, 500);
 	}
 
+	int x,y,w,h;
+
 	public void setup() {
 		colorMode(HSB);
 		background(0);
@@ -17,9 +19,28 @@ public class StarMap extends PApplet
 		
 	}
 
+	public void drawGrid(){
+		x=10;
+		y=10;
+		w=40;
+		h=40;
 		
+		fill(0);
+		for(int i =0; i< 11; i++) {
+			
+			for(int j =0; j< 11; j++){
+				rect(x,y,w,h);
+				x += w;
+			}
+			x=10;
+			y += h;
+		}
+	}	
+
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);
+		stroke(134);
+		drawGrid();		
 	}
 }

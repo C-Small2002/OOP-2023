19dde0a11cc18ee26a6d167ad02dbf5b28ba5ce8
Cathal Small <cathal.small1@gmail.com>
2023-04-24 15:46:25 +0100

    bla

---
+++ java/src/ie/tudublin/HealthPowerup.java
@@ -0,0 +1,25 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class HealthPowerup extends GameObject implements PowerUp{
+
+
+    public HealthPowerup(float x, float y, float rot, int c, YASC p){
+        super(x, y, rot, c, p);
+    }
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+
+}
